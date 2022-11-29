4a57c478fa55437492a0782a66db61c72adc6b0f
lazypenguin888 <115121070+lazypenguin888@users.noreply.github.com>
2022-11-29 16:20:40 -0500

    finished implementation

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-// TODO: create a component that displays a single bakery item
+import "../App.css";
+
 export function Item(props) {
     return (
-        <div>
-            <img src={props.item.image} alt="Italian Trulli"></img>
-            <p>{props.item.name}</p>
-            <p>About: {props.item.description}</p>
-            <p>Price: {props.item.price}</p>
+        <div className="item-box">
+            <img src={require("../assets/" + props.item.image)} alt={props.item.name} className="item-image"></img>
+            <h2>{props.item.name}</h2>
+            <h5>{props.item.description}</h5>
+            <h5>Price: {props.item.price}</h5>
+            {props.item.vegan === "yes" &&
+                <div className="tag" style={{background: "#ff617b"}}>vegan</div>
+            }
+            {props.item.glutenfree === "yes" &&
+                <div className="tag">gluten-free</div>
+            }
+            <br></br>
             <button onClick={() => props.addCart(props.item)}>Add To Cart</button>
         </div>
     )
